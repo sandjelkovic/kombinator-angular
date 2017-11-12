6b8d5c9e4ad93b90184a64cc1a8f75488923a2e0
stefan <sa.stefan.andjelkovic@gmail.com>
2017-11-12 12:51:41 +0100

    Update Angular and add basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {ContentComponent} from './content/content.component';
+import {FormsModule} from "@angular/forms";
+import {NavigationBarComponent} from './header/navigation-bar/navigation-bar.component';
+import {MainHeaderBarComponent} from './header/main-header-bar/main-header-bar.component';
+import {CombinationListComponent} from './content/combination-list/combination-list.component';
+import {CombinationComponent} from './content/combination/combination.component';
+import {CombinationItemComponent} from './content/combination-list/combination-item/combination-item.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {RouterModule} from "@angular/router";
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/take';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    NavigationBarComponent,
+    MainHeaderBarComponent,
+    CombinationListComponent,
+    CombinationComponent,
+    CombinationItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
